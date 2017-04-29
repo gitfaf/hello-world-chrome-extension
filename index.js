be38f32ffc2a7aeea7e81e8edc871c3b5aab603d
Git Faf <git.faf@gmail.com>
2017-04-30 03:50:08 +0530

    Hello, World
    Signed-off-by: Git Faf <git.faf@gmail.com>

---
+++ index.js
@@ -0,0 +1,23 @@
+var EVENT_DOM_CONTENT_LOADED = 'DOMContentLoaded';
+
+function getGreetingId () {
+  return 'greeting';
+}
+
+function getGreeting() {
+  return 'Hello, World!';
+}
+
+function getGreetingElement () {
+  return document.getElementById(getGreetingId());
+}
+
+function renderGreeting () {
+  getGreetingElement().textContent = getGreeting();
+}
+
+function fireWhenDOMContentIsLoaded () {
+  renderGreeting();
+}
+
+document.addEventListener(EVENT_DOM_CONTENT_LOADED, fireWhenDOMContentIsLoaded);
